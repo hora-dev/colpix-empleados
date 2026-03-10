703f1062412b13036ad4a48cfc7200c86d8afe47
horadev <horacio@megawebs.com.ar>
2026-03-10 02:55:36 -0300

    swagger

--- src/main/java/com/colpix/empleados/infraestructure/configuration/SecurityConfig.java
+++ src/main/java/com/colpix/empleados/infraestructure/configuration/SecurityConfig.java
@@ -21,7 +21,11 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests( auth ->
-                        auth.requestMatchers("/api/v1/auth/login").permitAll()
+                        auth.requestMatchers("/api/v1/auth/login",
+                                        "/v3/api-docs/**",
+                                        "/swagger-ui/**",
+                                        "/swagger-ui.html").permitAll()
+
                                 .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
